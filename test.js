5f6dd26e871443f16882d7e994ad64974578e686
Maksymilian Januszewski <m.januszewski.517@studms.ug.edu.pl>
2025-01-02 22:53:01 +0100

    Structuring starting page for logging and register and changes

--- test.js
+++ test.js
@@ -7,11 +7,28 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.static("public"));
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 
+app.get("/", (req, res) => {
+    res.render("start.ejs")
+})
 
 
+app.get("/auth", (req, res) => {
+    res.render("auth.ejs")
+})
 
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard.ejs")
+})
+
+
+app.get("/profile", (req, res) => {
+    res.render("profile.ejs")
+})
 
 
 
